8562f9fe3f549cfdc0cbbb9aa123534e50c2a553
eddemorgan <eddemorg@gmail.com>
2026-06-03 16:27:02 -0500

    add favicon to website

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
         <meta property="og:title" content="Collective Loft" />
         <meta property="og:description" content="Building the infrastructure meant for the creative class." />
         <meta property="og:url" content="https://collectiveloft.com" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,600;1,300;1,400&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" />
